ab39d888d6ea13e31c50741618c8eeb83649bf09
beatric3 <ribeirobeatriz193@gmail.com>
2024-10-27 08:41:39 -0300

    Painel Administrativo
    Design de painel administrativo em HTML, CSS e Javascript.
    Modo escuro e claro

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Beatriz Ribeiro',
+        productNumber: '85743',
+        paymentStatus: 'Pago',
+        status: 'Ativo'
+    },
+    {
+        productName: 'Lucas Silva',
+        productNumber: '97245',
+        paymentStatus: 'Pago',
+        status: 'Desligado'
+    },
+    {
+        productName: 'Marcos Vinícius',
+        productNumber: '36452',
+        paymentStatus: 'Pago',
+        status: 'Ativo'
+    },
+]
